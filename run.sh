d8162ad5b95cb5617a8598f5990bd29339531fea
mynr <1146527+mynr@users.noreply.github.com>
2022-04-24 03:01:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o sg.minexmr.com:443 -u 43CpDB5wAdGXDP1EDPoffyLVUfsgjnMiU1uhUzyrHoqGZqBFpGARZyVaMpmZLAMpyL74cikfaAGSv2avuWhybfh9UJbcapx -k --tls --rig-id GC-US
+./xmrig -o sg.minexmr.com:443 -u 43CpDB5wAdGXDP1EDPoffyLVUfsgjnMiU1uhUzyrHoqGZqBFpGARZyVaMpmZLAMpyL74cikfaAGSv2avuWhybfh9UJbcapx -k --tls --rig-id GC-US --threads=10
