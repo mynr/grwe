f53877536c54f0127b032ab478c8f7786bbdd616
mynr <1146527+mynr@users.noreply.github.com>
2022-04-24 01:29:35 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
+##! /bin/bash
+## Install XRDP
+## Before get xmr coin for free
+## Google Colab
 sudo apt update
 clear
-
+sudo apt install screen
+screen -R xmr
+ 
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
