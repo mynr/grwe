0a8c2b4deb27430740467cd19904963e3dad2104
mynr <1146527+mynr@users.noreply.github.com>
2022-04-24 04:52:05 +0700

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,8 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
